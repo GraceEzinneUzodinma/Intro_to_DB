e812e43cb67cf73b02eb1b795f0591c33cceaae4
grace uzodinma <guzodinma2@gmail.com>
2025-10-15 12:03:50 +0100

    book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books(
-    book_id (Primary Key),
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id (Foreign Key referencing Authors table),
     price DOUBLE,
@@ -11,18 +11,18 @@ CREATE TABLE Authors(
     author_name VARCHAR(215)
 );
 CREATE TABLE Customers(
-    customer_id (Primary Key),
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 CREATE TABLE Orders(
-    order_id (Primary Key),
+    order_id INT PRIMARY KEY,
     customer_id (Foreign Key referencing Customers table),
     order_date DATE
 );
 CREATE TABLE Order_details(
-    orderdetailid (Primary Key),
+    orderdetailid INT PRIMARY KEY,
     order_id (Foreign Key referencing Orders table),
     book_id (Foreign Key referencing Books table),
     quantity DOUBLE
