9058f4935e4d25e9b80f17ba2ff19119f26d027f
grace uzodinma <guzodinma2@gmail.com>
2025-10-17 15:40:14 +0100

    list

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,8 @@
 USE alx_book_store;
-SELECT * FROM Books;
+SELECT
+    COLUMN_NAME AS "column",
+    COLUMN_TYPE AS "Type",
+
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books'
