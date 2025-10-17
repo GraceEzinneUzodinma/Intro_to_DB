f41c415fee10ea7ff89eac36423116c8b8cde2b1
grace uzodinma <guzodinma2@gmail.com>
2025-10-17 14:03:37 +0100

    creating tables

---
+++ task_2.sql
@@ -0,0 +1,29 @@
+USE alx_book_store;
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id (Foreign Key referencing Authors table),
+    price DOUBLE,
+    publication_date DATE
+);
+CREATE TABLE Authors(
+    author_id (Primary Key),
+    author_name VARCHAR(215)
+);
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id (FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)),
+    order_date DATE
+);
+CREATE TABLE Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id (FOREIGN KEY (order_id) REFERENCES Orders(order_id)),
+    book_id (FOREIGN KEY (book_id) REFERENCES Books(book_id)),
+    quantity DOUBLE
+);
